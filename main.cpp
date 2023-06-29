d0857bc18b6e1b507afcd1f5ebba73c14b79873b
Tarbanov <ttarbanov@gmail.com>
2023-06-29 22:38:52 +0200

    ...

--- main.cpp
+++ main.cpp
@@ -1,3 +1,41 @@
 #include <iostream>
 using namespace std;
 
+int chooseNumber(int number){
+    int num;
+    cin >> num;
+}
+
+
+int main() {
+    cout<<"Selec an action: "<<endl<<endl;
+    cout<<"1)A* algorithm" << endl;
+    cout<<"2)Cancel"<<endl;
+
+    int num1;
+    cin>>num1;
+    while(num1<=0 || num1>2){
+        cout <<"Set the correct number"<<endl;
+        cin.clear();
+        cin>> num1;
+    }
+
+    if(num1 == 1){
+
+
+
+    }
+
+    if(num1==2){
+
+
+
+
+    }
+
+
+
+
+
+
+}
