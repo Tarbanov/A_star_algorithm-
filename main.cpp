6e6448d66dd44e36683de124640a50bb69da9fa3
Tarbanov <ttarbanov@gmail.com>
2023-07-18 22:04:44 +0200

    update

--- main.cpp
+++ main.cpp
@@ -9,33 +9,59 @@ struct Node{
     int x; //X coordinate
     int y; //Y coordinate
     int g; // Cost from the starting point
-    int h; //Distance estimation
-    int f; //f=g+h
+    double h; //Distance estimation
+    double f; //f=g+h
 
-    Node(int x, int y, int g, int h,int f): x(x), y(y), g(g), h(h), f(f) {
+    Node(int x, int y, int g, double h,double f): x(x), y(y), g(g), h(h), f(f) {
 
     }
 };
 
 //h
-int heuristic(int x_S,int y_S,int x_F, int y_F){
-    int h=sqrt((x_S-x_F)*(x_S-x_F)+(y_S-y_F)*(y_S-y_F));
+double heuristic(int x_S,int y_S,int x_F, int y_F){
+    double h=sqrt(((x_S-x_F)*(x_S-x_F))+((y_S-y_F)*(y_S-y_F)));
     return h;
 }
 
 
+void path (int x_S, int y_S,int x_F, int y_F, vector<vector<char>>&v,int row,int col){
 
-
-void path (int x_S, int y_S,int x_F, int y_F, vector<vector<char>>&v){
-    vector<int> dx = {0,0,1,-1};
-    vector<int> dy = {1,-1,0,0};
+    vector<int> dx = {2,-2,0,0};
+    vector<int> dy = {0,0,1,-1};
     Node current(x_S,y_S,0, heuristic(x_S,y_S,x_F,y_F),heuristic(x_S,y_S,x_F,y_F));
 
-    for(int i=0;i<4;i++){
-       int newX=current.x+dx[i];
-       int newY=current.y+dy[i];
+//    vector<vector<int>> gScore(row,vector<int>(col));
+//    gScore[x_S][y_S]=0;
 
+    int k=x_S;
+    int l=y_S;
+    while(v[k][l]!='F'){
+         for(int i=0;i<4;i++) {
+             int newX = current.x + dx[i];
+             int newY = current.y + dy[i];
 
+             int newG = current.g + 1;
+             int newF = heuristic(newX, newY, x_F, y_F) + newG;
+
+             if(v[newX][newY]=='#'){
+                 break;
+             }
+
+             if (newF<current.f) {
+                 x_S=newX;
+                 y_S=newY;
+                 current.f=newF;
+                 current.g=newG;
+
+                 for(int n =0;n<row;n++){
+                     for(int m=0;m<col;m++){
+                         v[newX][newY]='@';
+                         cout << v[n][m];
+                     }
+                     cout << endl;
+                 }
+             }
+         }
     }
 
 
@@ -91,6 +117,8 @@ int main() {
         }
     }
 
+    cout << x_start << " " << y_start << endl;
+
     for(int i=0;i<rows;i++){
         for(int j=0;j<columns;j++){
             if(vecMap[i][j]=='F'){
@@ -102,17 +130,8 @@ int main() {
     }
 
 
-
     heuristic(x_start,y_start,x_finish,y_finish);
-    path(x_start,y_start,x_finish,y_finish,vecMap);
-
-
-
-
-
-    
-
-
+    path(x_start,y_start,x_finish,y_finish,vecMap,rows,columns);
 
 
 }
