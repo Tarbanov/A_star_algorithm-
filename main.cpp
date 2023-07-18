269ab0a4ff8cd3e37efb812c5751906d140c6fd0
Tarbanov <ttarbanov@gmail.com>
2023-07-18 16:40:49 +0200

    renovation

--- main.cpp
+++ main.cpp
@@ -5,44 +5,71 @@
 #include <cstring>
 using namespace std;
 
-struct Node{
+//struct Node{
+//    int x; //X coordinate
+//    int y; //Y coordinate
+//    int g; // Cost from the starting point
+//    int h; //Distance estimation
+//    int f; //f=g+h
+//
+//};
 
 
-
-
-};
 
 int main() {
+    int x=0;
+    int y=0;
 
     string line;
-    char arrMap[200];
-
-    //чтение файла-карты
+    vector<vector<char>> vecMap;
 
     ifstream map("map1.txt");
-    if (map.is_open())
-    {
-        while(getline(map, line)) {
-            strcpy( arrMap, line.c_str() );
-            int i=0;
-            while(arrMap[i]){
-                if(arrMap[i]=='1'){
-                    arrMap[i]='#';
-                }
-                if(arrMap[i]=='0'){
-                    arrMap[i]='.';
-                }
-                cout << arrMap[i];
-                i++;
-            }
-            cout << endl;
+    while(getline(map,line)){
+        vector<char> row;
+        for(char &c : line){
+            row.push_back(c);
         }
-        map.close();
+        vecMap.push_back(row);
     }
-    else cout << "Unable to open map1";
+
+    int rows=0;
+    int columns =0;
+    for(vector<char> &row : vecMap){
+        columns=0;
+        for(char &c: row){
+
+            if(c=='1'){
+                c='#';
+            }
+            if(c=='0'){
+                c='.';
+            }
+            cout << c;
+            columns++;
+        }
+        rows++;
+        cout << endl;
+    }
+
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            if(vecMap[i][j]=='F'){
+                x=i;
+                y=j;
+                break;
+            }
+        }
+    }
+
+    cout<< x << " " << y << endl;
 
 
 
+
+
+
+
+    
 
 
 
