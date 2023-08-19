c586e43d80eca5ba614e30a3c85c2a22e4b67a72
Tarbanov <ttarbanov@gmail.com>
2023-08-19 15:03:28 +0200

    upgrade /the user can upload his map

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 struct Node{
     int x; //X coordinate
     int y; //Y coordinate
-    int g; // Cost from the starting point
+    double g; // Cost from the starting point
     double h; //Distance estimation
     double f; //f=g+h
 
@@ -47,7 +47,7 @@ void path (int x_S, int y_S,int x_F, int y_F, vector<vector<char>>&v,int row,int
         for(int i=0;i<4;++i) {
             int newX = current.x + dx[i];
             int newY = current.y + dy[i];
-            double newG = current.g + 0.5;//the cost of 1 move = 0.5
+            double newG = current.g + 0.7;//the cost of 1 move = 0.5
             double newF = heuristic(newX, newY, x_F, y_F) + newG;
 
             //I make sure that I don't go beyond the boundaries
@@ -83,6 +83,7 @@ void path (int x_S, int y_S,int x_F, int y_F, vector<vector<char>>&v,int row,int
 
 
         current.f=fhelper[num];
+
         current.g=ghelper[num];
 
         //writing a new move
@@ -109,8 +110,8 @@ void path (int x_S, int y_S,int x_F, int y_F, vector<vector<char>>&v,int row,int
         index--;
 
 
-        unsigned int microsecond = 500000;
-        usleep(  microsecond);//sleeps for 0.5 second
+        unsigned int microsecond = 100000;
+        usleep(  0.5*microsecond);//sleeps for 0.5 second
     }
 }
 
@@ -150,12 +151,13 @@ int main() {
     cout << "1) Map1(size: 18*42)" << endl;
     cout << "2) Map2(size: 25*30)" << endl;
     cout << "3) Map3(size: 39*74)" << endl;
+    cout << "4) Choose my own map" << endl;
     cout << endl;
 
     cout << "Choose a map: ";
     cin >> menu_map;
 
-    while(menu_map!=1 && menu_map!=2 && menu_map!=3){
+    while(menu_map!=1 && menu_map!=2 && menu_map!=3 && menu_map!=4){
         cout << "There is no such number, please enter the correct number:";
         cin.clear();
         cin.ignore(256, '\n');
@@ -206,45 +208,155 @@ int main() {
         }
     }
 
+    if(menu_map==4){
+        system("clear");
+        string name_own_map;
+        cout << "Enter file name:" <<endl;
+        cin >> name_own_map;
+        string file_name[8] ={"cmake-build-debug","a.out","CMakeLists.txt","documentation.txt","main.cpp","map1.txt","map2.txt","map3.txt"};
+        bool flag2 = true;
+
+        for (const auto & l : file_name){
+            if (name_own_map == l)
+                flag2 = false;
+        }
+
+        bool flag = false;
+
+        while(!flag) {
+            ifstream iff(name_own_map);
+            if (iff.good() and flag2){
+                flag= true;
+            } else {
+                cout << "There is no such name, please enter the correct name:";
+                cin.clear();
+                cin >> name_own_map;
+                flag2= true;
+                for (const auto & l : file_name){
+                    if (name_own_map == l)
+                        flag2 = false;
+                }
+
+            }
+        }
+
+        ifstream map(name_own_map);
+        while(getline(map,line)){
+            vector<char> row;
+            for(char &c : line){
+                if(c==' '){
+                    continue;
+                }
+                row.push_back(c);
+            }
+            vecMap.push_back(row);
+        }
+
+
+    }
+
 //Replacing numbers with signs 1==# , 0==.
     int rows=0;
     int columns =0;
-    for(vector<char> &row : vecMap){
-        columns=0;
-        for(char &c: row){
-            if(c=='1'){
-                c='#';
+    if(menu_map!=4) {
+        for (vector<char> &row: vecMap) {
+            columns = 0;
+            for (char &c: row) {
+                if (c == '1') {
+                    c = '#';
+                }
+                if (c == '0') {
+                    c = '.';
+                }
+                cout << c << " ";
+                columns++;
             }
-            if(c=='0'){
-                c='.';
-            }
-            cout << c << " ";
-            columns++;
+            rows++;
+            cout << endl;
         }
-        rows++;
-        cout << endl;
+        //find Start coordinates
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (vecMap[i][j] == 'S') {
+                    x_start = j;
+                    y_start = i;
+                    break;
+                }
+            }
+        }
+
+        //find Finish coordinates
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (vecMap[i][j] == 'F') {
+                    x_finish = j;
+                    y_finish = i;
+                    break;
+                }
+            }
+        }
     }
-   //find Start coordinates
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-            if(vecMap[i][j]=='S'){
-                x_start=j;
-                y_start=i;
-                break;
+    else{
+        system("clear");
+
+        for (vector<char> &row: vecMap) {
+            columns = 0;
+            for (char &c: row) {
+                if (c == '1') {
+                    c = '#';
+                }
+                if (c == '0') {
+                    c = '.';
+                }
+                cout << c << " ";
+                columns++;
             }
+            rows++;
+            cout << endl;
         }
+
+
+        cout << rows << columns << endl;
+        cout << "set start coordinates" << endl;
+        cout << "x-axis: ";
+        cin >> x_start;
+        while (x_start<0 or x_start >=columns){
+            cout << "set a number between 0 and " << columns-1 << endl;
+            cin.clear();
+            cin >> x_start;
+        }
+        cout << "y-axis: ";
+        cin >> y_start;
+        while (y_start<0 or y_start >=rows){
+            cout << "set a number between 0 and " << rows-1 << endl;
+            cin.clear();
+            cin >> y_start;
+        }
+        cout << "set finish coordinates" << endl;
+        cout << "x-axis: ";
+        cin >> x_finish;
+        while (x_finish<0 or x_finish >=columns){
+            cout << "set a number between 0 and " << columns-1 << endl;
+            cin.clear();
+            cin >> x_finish;
+        }
+        cout << "y-axis: ";
+        cin >> y_finish;
+        while (y_finish<0 or y_finish >=rows){
+            cout << "set a number between 0 and " << rows-1 << endl;
+            cin.clear();
+            cin >> y_finish;
+        }
+
+        vecMap[y_start][x_start]='S';
+        vecMap[y_finish][x_finish]='F';
+
+
+
+
     }
 
-    //find Finish coordinates
-    for(int i=0;i<rows;i++){
-        for(int j=0;j<columns;j++){
-            if(vecMap[i][j]=='F'){
-                x_finish=j;
-                y_finish=i;
-                break;
-            }
-        }
-    }
+
 
     path(x_start,y_start,x_finish,y_finish,vecMap,rows,columns);
 
